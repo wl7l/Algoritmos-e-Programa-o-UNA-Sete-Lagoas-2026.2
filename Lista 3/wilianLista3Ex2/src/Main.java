f08f1c15a60a48e42970ecbecf7984fbc7152ef9
wl7l <159781267+wl7l@users.noreply.github.com>
2026-09-15 16:06:37 -0300

    Add files via upload

---
+++ Lista 3/wilianLista3Ex2/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 1; i--) {
+            System.out.println("Contagem: " + i); }
+                System.out.println("Decolar!");
+        }
+    }
---
+++ Lista 3/wilianLista3Ex3/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 20; i += 2) {
+            System.out.println("Numeros Pares: " + i);
+        }
+    }
+}
