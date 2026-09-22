e6de9b5589b4c77487c1295fdf99ad69c9074fc2
wl7l <159781267+wl7l@users.noreply.github.com>
2026-09-22 11:51:17 -0300

    Add files via upload

--- Lista 3/wilianLista3Ex6/src/Main.java
+++ Lista 3/wilianLista3Ex6/src/Main.java
@@ -5,19 +5,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-            int senha_correta = 2026;
+            String senha = "0";
+            String senha_correta = "2026";
             int tentativas = 0;
 
         System.out.print("Digite uma senha inteira de 4 dígitos: ");
-        int digitada = input.nextInt();
-        tentativas++;
+        String senha_digitada = input.nextLine();
+        tentativas++; // Essa primeira digitação vai contar como uma tentativa.
 
-        while (digitada != senha_correta) {
+        while (!senha_digitada.equals(senha_correta)) { // .equals usado para a string.
             System.out.println("Senha Incorreta, Tente novamente!");
             System.out.println("Digite a senha de 4 dígitos: ");
-            digitada = input.nextInt();
+            senha_digitada = input.nextLine();
             tentativas++;
-
         }
 
         System.out.println("Acesso Autorizado!");
